5cb7ffb318b5fbf6604cca2e64ec06fa03e6cdbb
garg1337 <ArjunPGarg@gmail.com>
2022-01-04 21:31:08 -0600

    formatted print exercise

---
+++ formatted-print/target/debug/deps/formatted_print.d
@@ -0,0 +1,5 @@
+C:\Users\Arjun\Documents\rust-by-example\formatted-print\target\debug\deps\formatted_print.exe: src\main.rs
+
+C:\Users\Arjun\Documents\rust-by-example\formatted-print\target\debug\deps\formatted_print.d: src\main.rs
+
+src\main.rs:
---
+++ formatted-print/target/debug/formatted-print.d
@@ -0,0 +1,1 @@
+C:\Users\Arjun\Documents\rust-by-example\formatted-print\target\debug\formatted-print.exe: C:\Users\Arjun\Documents\rust-by-example\formatted-print\src\main.rs
